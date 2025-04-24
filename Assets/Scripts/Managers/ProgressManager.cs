3b18193a2642b3169df0f6b364b6c9b6c0ebe817
JoelVRV <joelcospfp@ibf.cat>
2025-04-25 01:26:18 +0200

    PROBANDO MIERDAS PROGRESS MANAGER

--- Assets/Scripts/Managers/ProgressManager.cs
+++ Assets/Scripts/Managers/ProgressManager.cs
@@ -5,12 +5,17 @@ using System.IO;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEditor;
 
+// Objeto a guardar en el JSON y que contiene todas la data
+public class ProgressData
+{
+    public bool tutorial;
+}
+
 public class ProgressManager : MonoBehaviour
 {
-
     public static ProgressManager Instance { get; private set; }
-
-    private string filePath;
+    ProgressData progressData;
+    string saveFilePath;
 
     private void Awake()
     {
@@ -27,22 +32,51 @@ public class ProgressManager : MonoBehaviour
 
     void Start()
     {
-        filePath = Application.dataPath + "/ProgressManager.json";
-        if (File.Exists(filePath))
+        progressData = new ProgressData(); // Instancia del ProgressData
+        progressData.tutorial = true;
+  
+        saveFilePath = Application.persistentDataPath + "/ProgressManager.json"; // Ruta del JSON
+
+        if (File.Exists(saveFilePath))
         {
-            Debug.Log("------> ARCHIVO EXISTENTE");
+            string loadProgressData = File.ReadAllText(saveFilePath);
+            progressData = JsonUtility.FromJson<ProgressData>(loadProgressData);
         }
         else
         {
-            WriteData();
+            string savePlayerData = JsonUtility.ToJson(progressData);
+            File.WriteAllText(saveFilePath, savePlayerData);
+            Debug.Log("Save file created at: " + saveFilePath);
         }
     }
-
-    public void WriteData()
+    public void SaveGame()
     {
-        string word = "¡Hello World!";
-        File.WriteAllText(Application.dataPath +"/ProgressManager.json", word);
-        Debug.Log("------> ARCHIVO CREADO");
-
+        string savePlayerData = JsonUtility.ToJson(progressData);
+        File.WriteAllText(saveFilePath, savePlayerData);
+        Debug.Log("Save file created at: " + saveFilePath);
+    }
+    public void LoadGame()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string loadProgressData = File.ReadAllText(saveFilePath);
+            progressData = JsonUtility.FromJson<ProgressData>(loadProgressData);
+        }
+        else
+        {
+            Debug.Log("There is no save files to load!");
+        }
+    }
+    public void DeleteSaveFile()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            File.Delete(saveFilePath);  
+            Debug.Log("Save file deleted!");
+        }
+        else
+        {
+            Debug.Log("There is nothing to delete!");
+        }
     }
 }
