f6500ac40e0ab51f15cb999ae3fb0212fadd7cd7
JoelDAW2 <145133218+JoelDAW2@users.noreply.github.com>
2025-04-30 20:24:21 +0200

    PROBANDO DOS TORRES

--- Assets/Scripts/Enemies/GenericEnemy/EnemyAreaConfigurator.cs
+++ Assets/Scripts/Enemies/GenericEnemy/EnemyAreaConfigurator.cs
@@ -3,7 +3,7 @@ using UnityEngine.AI;
 
 public class EnemyAreaConfigurator : MonoBehaviour
 {
-    [SerializeField] private Element activeElement;
+    [SerializeField] public Element activeElement;
 
     private int oppositeAreaIndex;
     private NavMeshAgent agent;
