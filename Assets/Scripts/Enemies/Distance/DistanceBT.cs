e56c3ff7321599c1fcd92da2e1f6cb7493349890
polcastellano <polcj15@gmail.com>
2025-05-11 23:28:58 +0200

    Arreglando enemigo de AGUA, ataque funciona y perseguir tambien y el drop de la orbe

--- Assets/Scripts/Enemies/Distance/DistanceBT.cs
+++ Assets/Scripts/Enemies/Distance/DistanceBT.cs
@@ -153,7 +153,7 @@ public class DistanceBT : Enemy
         }
         else
         {
-            Destroy(this);
+            Dying();
         }
     }
     private void SetLookingPlayersActive(bool active)
