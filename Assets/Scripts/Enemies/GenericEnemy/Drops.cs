6f869bdaf87d2587a148d89c82c62b40bfa6d06f
akatrebla <albertgarciafp24@ibf.cat>
2025-05-14 12:42:35 +0200

    Hub Element

--- Assets/Scripts/Enemies/GenericEnemy/Drops.cs
+++ Assets/Scripts/Enemies/GenericEnemy/Drops.cs
@@ -5,40 +5,13 @@ using UnityEngine;
 public class Drops : MonoBehaviour
 {
     public Element element;
-    private int min = 40;
-    private int max = 60;
+
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(Constants.player))
         {
-            if (element == Element.None)
-            {
-                other.GetComponent<VikingController>().earthMana = 100;
-                other.GetComponent<VikingController>().waterMana = 100;
-                other.GetComponent<VikingController>().fireMana = 100;
-                other.GetComponent<VikingController>().electricMana = 100;
-            }
-            else if (element == Element.Earth)
-            {
-                other.GetComponent<VikingController>().earthMana += Random.Range(min,max);
-                if (other.GetComponent<VikingController>().earthMana > 100) other.GetComponent<VikingController>().earthMana = 100;
-            }
-            else if (element == Element.Water)
-            {
-                other.GetComponent<VikingController>().waterMana += Random.Range(min,max);
-                if (other.GetComponent<VikingController>().waterMana > 100) other.GetComponent<VikingController>().waterMana = 100;
-            }   
-            else if (element == Element.Fire)
-            {
-                other.GetComponent<VikingController>().fireMana += Random.Range(min,max);
-                if (other.GetComponent<VikingController>().fireMana > 100) other.GetComponent<VikingController>().fireMana = 100;
-            }   
-            else if (element == Element.Electric)
-            {
-                other.GetComponent<VikingController>().electricMana += Random.Range(min,max);
-                if (other.GetComponent<VikingController>().electricMana > 100) other.GetComponent<VikingController>().electricMana = 100;
-            }      
+            other.GetComponent<VikingController>().CollectMana(element);  
             Destroy(gameObject);
         }
     }
