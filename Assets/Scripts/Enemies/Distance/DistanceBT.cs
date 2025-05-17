9a0c15e08b8c9dcc73f745a4e25dc55f07517b7b
PauSubirada <psubirada23@gmail.com>
2025-05-17 02:23:22 +0200

    Quitar llamada reset cooldown heavy attack para dejarlo como la version del pol

--- Assets/Scripts/Enemies/Distance/DistanceBT.cs
+++ Assets/Scripts/Enemies/Distance/DistanceBT.cs
@@ -101,7 +101,6 @@ public class DistanceBT : Enemy
                                     {
                                         //transform.LookAt(player.transform);
                                         animator.SetInteger(Constants.state, 3);
-                                        ResetHeavyAttackCooldown();
                                     }
                                     else
                                     {
