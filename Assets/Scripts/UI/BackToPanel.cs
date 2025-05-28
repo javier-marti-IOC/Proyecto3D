6580ee278707af0d5962a886fcfc6d1fc82cc64c
DavidIESBernat <145132895+DavidIESBernat@users.noreply.github.com>
2025-05-28 19:26:39 +0200

    InputManager en menus totalmente funcional
    Arreglados varios bugs en los menus tanto de pausa como principal con el nuevo sistema de InputManager.

--- Assets/Scripts/UI/BackToPanel.cs
+++ Assets/Scripts/UI/BackToPanel.cs
@@ -8,43 +8,53 @@ using UnityEngine.InputSystem;
 public class BackToPanel : MonoBehaviour
 {
     public GameObject panelToBack; // Panel objetivo al que se quiere volver
-    public PauseMenu pauseMenu; // Script menu pausa
+    //public GameObject pausePanel;
+    //public PauseMenu pauseMenu; // Script menu pausa
     public Button selectedButton; // Boton seleccionado por defecto al cargar el panel anterior
-    private InputAction cancelAction;
+    //private InputAction cancelAction;
 
-    void OnEnable()
-    {
-        // Obtener la acción "Cancel" del mapa de acciones activo
-        PlayerInput uiInput = FindObjectOfType<PlayerInput>();
-        cancelAction = uiInput.actions["Cancel"];
-        cancelAction.performed += OnCancel;
-        cancelAction.Enable();
-    }
+    // void OnEnable()
+    // {
+    //     // Obtener la acción "Cancel" del mapa de acciones activo
+    //     PlayerInput uiInput = FindObjectOfType<PlayerInput>();
+    //     cancelAction = uiInput.actions["Cancel"];
+    //     cancelAction.performed += OnCancel;
+    //     cancelAction.Enable();
+    // }
 
-    void OnDisable()
-    {
-        if (cancelAction != null)
-        {
-            cancelAction.performed -= OnCancel;
-            cancelAction.Disable();
-        }
-    }
+    // void OnDisable()
+    // {
+    //     if (cancelAction != null)
+    //     {
+    //         cancelAction.performed -= OnCancel;
+    //         cancelAction.Disable();
+    //     }
+    // }
 
-    private void OnCancel(InputAction.CallbackContext context)
+    public void GoBack()
     {
         if (panelToBack != null)
         {
-            panelToBack.SetActive(true); // Mostrar panel anterior
-            gameObject.SetActive(false); // Ocultar panel actual
-            selectedButton.Select();     // Seleccionar botón predeterminado
-        }
-        else if (pauseMenu != null) // Cerrar el menu de pausa
-        {
-            pauseMenu.TogglePause();
-        }
-        else
-        {
-            Debug.LogWarning("Panel no asignado en el Inspector.");
+            panelToBack.SetActive(true);
+            gameObject.SetActive(false);
+            selectedButton.Select();
         }
     }
+    // private void OnCancel(InputAction.CallbackContext context)
+    // {
+    //     if (panelToBack != null)
+    //     {
+    //         panelToBack.SetActive(true); // Mostrar panel anterior
+    //         gameObject.SetActive(false); // Ocultar panel actual
+    //         selectedButton.Select();     // Seleccionar botón predeterminado
+    //     }
+    //     else if (pausePanel != null && pausePanel.activeSelf) // Solo para cerrar el menu de pausa
+    //     {
+    //         pauseMenu.TogglePause();
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("Panel no asignado en el Inspector.");
+    //     }
+    // }
 }
