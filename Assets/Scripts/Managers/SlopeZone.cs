c1fd10b2dddf4dd77c149ef6f168c1f3608cf955
javilevel <javimartiruiz@gmail.com>
2025-06-04 18:02:28 +0200

    Slope manager updated

--- Assets/Scripts/Managers/SlopeZone.cs
+++ Assets/Scripts/Managers/SlopeZone.cs
@@ -3,26 +3,45 @@ using UnityEngine;
 public class SlopeZone : MonoBehaviour
 {
     [Tooltip("Nuevo slope limit mientras el jugador esté dentro de esta zona")]
-    private bool isFirstTimeEnter;
-    private bool isFirstTimeExit;
+    public float zoneSlopeLimit = 55f;
+
+    private bool initialized = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!initialized && other.CompareTag(Constants.player))
+        {
+            var handler = other.GetComponent<CharacterSlopeHandler>();
+            if (handler != null)
+            {
+                handler.EnterSlopeZone(zoneSlopeLimit);
+                initialized = true;
+            }
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(Constants.player) && isFirstTimeEnter)
+        if (other.CompareTag(Constants.player))
         {
-            isFirstTimeEnter = false;
-            isFirstTimeExit = true;
-            other.GetComponent<CharacterController>().slopeLimit = 55;
+            var handler = other.GetComponent<CharacterSlopeHandler>();
+            if (handler != null)
+            {
+                handler.EnterSlopeZone(zoneSlopeLimit);
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag(Constants.player) && isFirstTimeExit)
+        if (other.CompareTag(Constants.player))
         {
-            isFirstTimeEnter = true;
-            isFirstTimeExit = false;
-            other.GetComponent<CharacterController>().slopeLimit = 55;
+            var handler = other.GetComponent<CharacterSlopeHandler>();
+            if (handler != null)
+            {
+                handler.ExitSlopeZone();
+            }
         }
     }
 }
